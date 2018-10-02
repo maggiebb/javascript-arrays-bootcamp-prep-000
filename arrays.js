9649ee660c1f9325d87450972db09358e5cc0ba3
Margaret Brett-Bowen <maggie.erickson@gmail.com>
2018-10-02 18:09:32 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,35 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+
+function addElementToBeginningOfArray(myArray, element){
+  var newArray = [element,...myArray];
+  return newArray;
+  
+}
+
+function destructivelyAddElementToBeginningOfArray(myArray,element){
+  myArray.unshift(element);
+  return myArray;
+}
+
+function addElementToEndOfArray(myArray, element){
+  var newArray = [...myArray, element];
+  return newArray;
+  
+}
+
+function destructivelyAddElementToEndOfArray(myArray,element){
+  myArray.push(element);
+  return myArray;
+}
+
+function accessElementInArray(array,index){
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray (array){
+  array.shift();
+  return array;
+}
+
+
